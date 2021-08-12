58aac229d902012788d425b60eb2f00a11c2fd04
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-12 11:35:26 +0800

    initial commit with lab complete and test 50% width

---
+++ src/components/Counter.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>{count} Likes!</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+    </div>
+  );
+}
+
+export default Counter;
--- src/components/news-feed/NewsFeed.js
+++ src/components/news-feed/NewsFeed.js
@@ -1,24 +1,48 @@
 import React, { Component } from "react";
-
+import Counter from "../Counter";
+import "./NewsFeed.css";
 class NewsFeed extends Component {
   constructor() {
     super();
     this.state = {
-      articles: []
+      articles: [],
     };
   }
 
-  componentDidMount() {
-    const URL = `https://newsapi.org/v2/everything?sources=hacker-news&apiKey=${
-      process.env.REACT_APP_NEWSAPI_API_KEY
-    }`;
-    // TODO: start coding!
+  async componentDidMount() {
+    const URL = `https://newsapi.org/v2/everything?sources=hacker-news&apiKey=${process.env.REACT_APP_NEWSAPI_API_KEY}`;
+
+    try {
+      let resp = await fetch(URL);
+      let response = await resp.json();
+      this.setState({ articles: response.articles });
+
+      // .then((response) => response.json()) // convert data to json
+      // .then((json) => {
+      //   console.log(json);
+
+      //   this.setState({ articles: json.articles });
+      // }); // log data
+    } catch (err) {
+      console.log("ERROR" + err.message);
+    }
   }
+
+  // TODO: start coding!
 
   render() {
     return this.state.articles.map((article, i) => {
       // TODO: start coding!
-      return <li key={i}></li>;
+      return (
+        <div className="parent">
+          <Counter></Counter>
+          <p className="article-title">{article.title}</p>
+          <div className="article-time">
+            {new Date(Date.parse(article.publishedAt)).toLocaleString()}
+          </div>
+          <p className="article-content">{article.content}</p>
+        </div>
+      );
     });
   }
 }
